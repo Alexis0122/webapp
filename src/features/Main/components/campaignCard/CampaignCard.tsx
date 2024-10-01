a0e015afee046f63a1cef63cd9143737e0f3e4b6
Thunder04Wolf <pa.garcia0401@gmail.com>
2024-10-01 19:01:01 -0400

    refactor(styles): restructure styles and use native Mantine properties

--- src/features/Main/components/campaignCard/CampaignCard.tsx
+++ src/features/Main/components/campaignCard/CampaignCard.tsx
@@ -1,139 +1,95 @@
-import React, { useEffect, useState, useCallback } from 'react'
-import { Container } from '@mantine/core'
-import { CampaignCardItem } from './CampaignCard.utils' // Asegúrate de que la ruta sea correcta
-import { Carousel, Embla } from '@mantine/carousel'
-import '@mantine/carousel/styles.css'
+import React from 'react';
+import { Container } from '@mantine/core';
+import { CampaignCardItem } from './CampaignCard.utils';
+import { Carousel } from '@mantine/carousel';
+import '@mantine/carousel/styles.css';
+import classes from './CampaignCard.module.css';
 
 export const CampaignCard = () => {
-  const [embla, setEmbla] = useState<Embla | null>(null)
-
-  // Función para volver al primer slide al llegar al último
-  const handleScroll = useCallback(() => {
-    if (!embla) return
-
-    // Obtener el índice del slide actual y la cantidad de slides
-    const selectedIndex = embla.selectedScrollSnap()
-    const totalSlides = embla.scrollSnapList().length
-
-    // Si llegamos al último slide, volvemos al primero
-    if (selectedIndex === totalSlides - 1) {
-      setTimeout(() => embla?.scrollTo(1), 1000) // Vuelve al primer slide después de 1s
-    }
-  }, [embla])
-
-  useEffect(() => {
-    if (embla) {
-      embla.on('select', handleScroll) // Escucha cuando cambia de slide
-    }
-    return () => {
-      if (embla) {
-        embla.off('select', handleScroll) // Desconecta el evento al desmontar
-      }
-    }
-  }, [embla, handleScroll])
-
   return (
+    // Contenedor que centra el Carousel
     <Container
-      size='xs'
       style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        minHeight: '100vh',
-        width: '100px',
-        marginTop: '-70px'
+        display: 'flex', // Flexbox para centrar
+        justifyContent: 'center', // Centra horizontalmente
+        alignItems: 'center', // Centra verticalmente
+        minHeight: '100vh', // Ocupa el 100% del alto de la pantalla
+        width: '100vw', // Ocupa el 100% del ancho de la pantalla
       }}
     >
-      {/* Carrusel de Mantine */}
       <Carousel
-        getEmblaApi={setEmbla} // Obtiene la instancia de Embla
+        classNames={classes}
+        align="center"
         dragFree
-        loop // Activa el bucle
-        slideSize={{
-          base: '100%',
-          sm: '50%',
-          md: '33.333333%'
-        }}
-        height='auto'
+        loop
+        slideSize="25%" // Ajusta el tamaño de cada slide a un 25% del contenedor
         withIndicators
-        initialSlide={1} // Inicia en el primer slide
+        initialSlide={1}
+        slideGap="md" // Cambia el espacio entre los slides a `md`
+        controlsOffset="xl"
+        controlSize={50}
+        
         styles={{
-          indicator: {
-            width: 60,
-            height: 10,
-            transition: 'width 250ms ease',
-            '&[data-active]': {
-              width: 40
-            }
-          },
-          indicators: {
-            bottom: '-30px' // Ajusta esta propiedad para mover los indicadores más abajo
-          },
           container: {
-            maxWidth: '1600px',
-            width: '100%'
-          }
+            width: '1350px', // Asegura que el carrusel use el 100% del contenedor
+          },
         }}
       >
         {/* Slide 1 */}
-        <Carousel.Slide style={{ padding: '30px 0px 0px 0px' }}>
+        <Carousel.Slide style={{ padding: '30px 0' }}>
           <CampaignCardItem
             title='Pre-Launch'
             subtitle='Build Anticipation'
             buttonText='Starts Now'
             fees='Fees Free; 5% of funds raised from reservations'
             letter='A'
-            marginB={260}
-            paddingL='175px'
           />
         </Carousel.Slide>
 
         {/* Slide 2 */}
-        <Carousel.Slide style={{ padding: '0px 0px 0px 0px' }}>
+        <Carousel.Slide style={{ padding: '30px 0' }}>
           <CampaignCardItem
             title='Crowdfunding'
             subtitle='Raise Funds'
             buttonText='Start Your Campaign'
             fees='Fees 5%-8%; 4-5% of funds raised + transaction fees'
             letter='B'
-            sizeT='40px'
-            marginB={10}
-            marginT={310}
-            width='443px'
-            height='529px'
-            paddingL='150px'
           />
         </Carousel.Slide>
 
         {/* Slide 3 */}
-        <Carousel.Slide style={{ padding: '30px 0px 0px 0px' }}>
+        <Carousel.Slide style={{ padding: '30px 0' }}>
           <CampaignCardItem
             title='Learn More'
             subtitle='Explore Opportunities'
             buttonText='Learn More'
             fees='Fees 5%-8%; 4-5% of funds raised + transaction fees'
             letter='C'
-            marginB={260}
-            paddingL='175px'
           />
         </Carousel.Slide>
-        {/* Slide 2 */}
-        <Carousel.Slide style={{ padding: '0px 0px 0px 0px' }}>
+
+        {/* Slide 4 */}
+        <Carousel.Slide style={{ padding: '30px 0' }}>
           <CampaignCardItem
-            title='Crowdfunding'
-            subtitle='Raise Funds'
-            buttonText='Start Your Campaign'
+            title='Learn More'
+            subtitle='Explore Opportunities'
+            buttonText='Learn More'
             fees='Fees 5%-8%; 4-5% of funds raised + transaction fees'
-            letter='B'
-            sizeT='40px'
-            marginB={10}
-            marginT={310}
-            width='443px'
-            height='529px'
-            paddingL='150px'
+            letter='C'
+          />
+        </Carousel.Slide>
+
+        {/* Slide 5 */}
+        <Carousel.Slide style={{ padding: '30px 0' }}>
+          <CampaignCardItem
+            title='Learn More'
+            subtitle='Explore Opportunities'
+            buttonText='Learn More'
+            fees='Fees 5%-8%; 4-5% of funds raised + transaction fees'
+            letter='C'
           />
         </Carousel.Slide>
       </Carousel>
     </Container>
-  )
-}
+  );
+};
--- src/features/Main/components/campaignCard/CampaignCard.utils.tsx
+++ src/features/Main/components/campaignCard/CampaignCard.utils.tsx
@@ -1,21 +1,14 @@
 // CampaignCardItem.tsx
-import React from 'react'
-import { Card, Text, Button, Group, Divider, Box, Container } from '@mantine/core'
-import styles from './CampaignCard.module.css' // Import the CSS module
-import { IconSearch } from '@tabler/icons-react'
+import React from 'react';
+import { Card, Text, Button, Group, Divider, Container } from '@mantine/core';
+import styles from './CampaignCard.module.css';
 
 interface CampaignCardItemProps {
-  title: string
-  subtitle: string
-  buttonText: string
-  fees: string
-  letter?: string // Optional prop
-  marginB?: number // Optional prop
-  marginT?: number
-  sizeT?: string // Font size for title
-  width?: string // Custom width prop
-  height?: string // Custom height prop
-  paddingL?: string
+  title: string;
+  subtitle: string;
+  buttonText: string;
+  fees: string;
+  letter?: string;
 }
 
 export const CampaignCardItem: React.FC<CampaignCardItemProps> = ({
@@ -24,62 +17,28 @@ export const CampaignCardItem: React.FC<CampaignCardItemProps> = ({
   buttonText,
   fees,
   letter,
-  marginB = 260,
-  marginT = 0,
-  sizeT = '35px',
-  width = '400px', // Default width
-  height = '468px',
-  paddingL = '170px'
 }) => {
   return (
     <Container className={styles.container}>
-    <Card
-      shadow='md'
-      radius='md'
-      className={styles.card} // Use the card class from CSS
-      style={{ width, height }} // Set width and height
-    >
-      {letter && (
-        <Text
-          w={700}
-          size='xs'
-          fw='700'
-          className={styles.leftText} // Use leftText class
-          style={{ marginBottom: marginB, paddingLeft: paddingL }} // Keep marginB as a prop
-        >
-          {letter}. {subtitle}
+      <Card shadow="md" radius="md" className={styles.card}>
+        {letter && (
+          <Text w={700} size="xs" fw="700" className={styles.leftText}>
+            {letter}. {subtitle}
+          </Text>
+        )}
+        <Text w={700} size="35px" fw="700" className={styles.centeredTitle}>
+          {title}
         </Text>
-      )}
-      <Text w={700} size={sizeT} fw='700' className={styles.centeredTitle}>
-        {title}
-      </Text>
-      <Group justify='center' pb= '8px' className={styles.group}>
-        <Button
-          variant='filled'
-          fullWidth
-          className={styles.button}
-          size='xl'
-          style={{ marginTop: marginT }}
-        >
-          {buttonText}
-        </Button>
-      </Group>
-      <div style={{ width: '100%' }}>
-        {' '}
-
-        <Divider
-          orientation='horizontal'
-          color='black'
-          pb= '5px'
-          style={{ borderColor: 'black', borderWidth: '0.7px',  }} // Asegurar color y grosor
-          labelPosition='center'
-                 />
-      </div>
-
-      <Text className={styles.feesText} size='xs'>
-        {fees}
-      </Text>
-    </Card>
+        <Group justify="center" pb="8px" className={styles.group}>
+          <Button variant="filled" fullWidth className={styles.button}>
+            {buttonText}
+          </Button>
+        </Group>
+        <Divider orientation="horizontal" className={styles.customDivider} labelPosition="center" />
+        <Text className={styles.feesText} size="xs">
+          {fees}
+        </Text>
+      </Card>
     </Container>
-  )
-}
+  );
+};
