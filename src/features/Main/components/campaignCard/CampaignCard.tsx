77c0770ef942fca50f4b2c58323c6593c2b879d1
Thunder04Wolf <pa.garcia0401@gmail.com>
2024-10-02 20:33:08 -0400

    refactor(styles): restructure styles into module.css

--- src/features/Main/components/campaignCard/CampaignCard.tsx
+++ src/features/Main/components/campaignCard/CampaignCard.tsx
@@ -1,24 +1,13 @@
 // CampaignCard.tsx
 import React, { useState } from 'react'
 import { Carousel } from '@mantine/carousel'
-import { useMediaQuery } from '@mantine/hooks'
 import '@mantine/carousel/styles.css'
 import classes from './CampaignCard.module.css'
-import { CampaignCardItem } from './CampaignCard.utils' // Importa el componente que usarás para renderizar cada tarjeta
+import { CampaignCardItem } from './CampaignCard.utils'
+import { useMantineTheme } from '@mantine/core'
 
 export const CampaignCard = () => {
   const [activeSlide, setActiveSlide] = useState(0)
-
-  // Detectar tamaño de pantalla para cambiar diseño responsivo
-  const isLargeScreen = useMediaQuery('(min-width: 1024px)') // Pantallas grandes
-  const isMediumScreen = useMediaQuery('(min-width: 768px) and (max-width: 1023px)') // Pantallas medianas
-  const isSmallScreen = useMediaQuery('(max-width: 767px)');
-
-  // Definir el padding-left dinámico
-  const Ancho = isSmallScreen ? '95%' : isMediumScreen ? '75%' : '75%';
-  // Definir slideSize según tamaño de la pantalla
-  const slideSize = isLargeScreen ? '33.3333%' : isMediumScreen ? '50%' : '100%'
-  const widt = isLargeScreen ? '1000px' : isMediumScreen ? '500px' : '100px'
 
   // Datos de los slides
   const slides = [
@@ -28,13 +17,6 @@ export const CampaignCard = () => {
       buttonText: 'Starts Now',
       fees: 'Fees Free; 5% of funds raised from reservations',
       letter: 'A'
-    },
-    {
-      title: 'Learn More',
-      subtitle: 'Explore Opportunities',
-      buttonText: 'Learn More',
-      fees: 'Fees 5%-8%; 4-5% of funds raised + transaction fees',
-      letter: 'C'
     },
     {
       title: 'Crowdfunding',
@@ -51,51 +33,35 @@ export const CampaignCard = () => {
       letter: 'C'
     },
     {
-      title: 'Learn More',
-      subtitle: 'Explore Opportunities',
-      buttonText: 'Learn More',
+      title: 'Crowdfunding',
+      subtitle: 'Raise Funds',
+      buttonText: 'Start Your Campaign',
       fees: 'Fees 5%-8%; 4-5% of funds raised + transaction fees',
-      letter: 'C'
-    },
-    {
-      title: 'H',
-      subtitle: 'Build Anticipation',
-      buttonText: 'Starts Now',
-      fees: 'Fees Free; 5% of funds raised from reservations',
-      letter: 'A'
-    },
-    
+      letter: 'B'
+    }
   ]
+  console.log(`este es el  y este es el ${activeSlide}`)
+
 
   return (
     <div
-      style={{
-        width: '100%',
-        display: 'flex',            // Utiliza flexbox para centrar el contenedor
-        justifyContent: 'center',   // Centrado horizontal
-        alignItems: 'center',  
-      }}
+      className={classes.container}
     >
       <Carousel
         dragFree
         initialSlide={1}
-        w={Ancho}
+        className={classes.carousel}
         slideSize={{ base: '100%', sm: '50%', md: '33.333333%' }}
         slideGap={{ base: 0, sm: 'xl' }}
         loop
         align='center'
         onSlideChange={setActiveSlide}
-        m={50}
-
-
       >
-
-          
         {slides.map((slide, index) => (
           <Carousel.Slide
-            key={index} // Se mueve la key aquí
+            key={index}
             className={index === activeSlide ? classes.activeSlide : classes.inactiveSlide}
-           
+
           >
             <CampaignCardItem
               title={slide.title}
--- src/features/Main/components/campaignCard/CampaignCard.utils.tsx
+++ src/features/Main/components/campaignCard/CampaignCard.utils.tsx
@@ -1,7 +1,6 @@
-// CampaignCardItem.tsx
+// CampaignCard.tsx
 import React from 'react'
 import { Card, Text, Button, Group, Divider } from '@mantine/core'
-import { useMediaQuery } from '@mantine/hooks'
 import styles from './CampaignCard.module.css'
 
 interface CampaignCardItemProps {
@@ -10,7 +9,7 @@ interface CampaignCardItemProps {
   buttonText: string
   fees: string
   letter?: string
-  isActive?: boolean // Nuevo prop para indicar si es el card activo
+  isActive?: boolean // New prop to indicate if it is the active card
 }
 
 export const CampaignCardItem: React.FC<CampaignCardItemProps> = ({
@@ -19,62 +18,29 @@ export const CampaignCardItem: React.FC<CampaignCardItemProps> = ({
   buttonText,
   fees,
   letter,
-  isActive = false // Valor por defecto es false
+  isActive = false // Default value is false
 }) => {
-  // Definir media queries para detectar tamaños de pantalla
-  const isSmallScreen = useMediaQuery('(max-width: 767px)')
-  const isMediumScreen = useMediaQuery('(min-width: 768px) and (max-width: 1023px)')
-  const isLargeScreen = useMediaQuery('(min-width: 1024px)')
-
-  // Definir el padding-left dinámico
-  const dynamicPaddingLeft = isSmallScreen ? '230px' : isMediumScreen ? '170px' : '150px'
-  const cardWidth = isSmallScreen ? '300px' : isMediumScreen ? '430px' : '430px'
-  const cardHeight = isSmallScreen ? '420px' : isMediumScreen ? '550px' : '530px'
-  const marginTop = isSmallScreen ? '140px' : isMediumScreen ? '240px' : '240px'
-  const titleSize = isSmallScreen ? '30px' : isMediumScreen ? '40px' : '40px'
-  const buttonSize = isSmallScreen ? '20px' : isMediumScreen ? '40px' : '40px'
-
   return (
     <div>
-      <Card
-        shadow='md'
-        radius='md'
-        className={styles.card}
-        style={{ width: cardWidth, height: cardHeight }}
-      >
+      <Card shadow='md' radius='md' className={styles.card}>
         {letter && (
-          <Text
-            w={700}
-            size='xs'
-            fw='700'
-            pl={dynamicPaddingLeft} // Usar padding-left dinámico según tamaño de pantalla
-            className={styles.leftText}
-          >
+          <Text w={700} size='xs' fw='700' className={styles.leftText}>
             {letter}. {subtitle}
           </Text>
         )}
-        <Text
-          mt='20px'
-          w={700}
-          size={titleSize}
-          // size={isActive ? '59px' : '40px'}
-          fw='700'
-          className={styles.centeredTitle}
+        <Text 
+          mt='20px' 
+          w={700} 
+          fw='700' 
+          className={`${styles.centeredTitle} ${isActive ? styles.activeTitle : ''}`} // Conditional class
         >
           {title}
         </Text>
         <Group justify='center' pb='8px' className={styles.group}>
           <Button
-            mt={marginTop}
-            // mt={isActive ? '300px' : '240px'}
             variant='filled'
             fullWidth
             className={styles.button}
-            styles={{
-              root: {
-                fontSize: buttonSize // Aumentar el tamaño de la fuente del botón
-              }
-            }}
           >
             {buttonText}
           </Button>
