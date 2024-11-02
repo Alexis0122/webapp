0eb0c75302cb2e3b177760a3cb368f19164ad644
Alexis Ramirez <alexisramirez0122@hotmail.com>
2024-11-02 15:43:53 -0400

    Ajustandndo el axios

--- src/features/Main/components/CampaignLanding/CampaignLanding.tsx
+++ src/features/Main/components/CampaignLanding/CampaignLanding.tsx
@@ -6,13 +6,13 @@ import { Star } from "@phosphor-icons/react";
 export const CampaignLanding = () => {
   return (
     <Stack align="center">
-      <Tabs defaultValue="Pre-Launch" variant='pills' radius="md">
+      {/* <Tabs defaultValue="Pre-Launch" variant='pills' radius="md">
         <Tabs.List>
           <Tabs.Tab leftSection={<Star size={12} />} value="Pre-Launch">Pre-Launch</Tabs.Tab>
           <Tabs.Tab leftSection={<Star size={12} />} value="Crowdfunding">Crowdfunding</Tabs.Tab>
           <Tabs.Tab leftSection={<Star size={12} />} value="in-demand">in demand</Tabs.Tab>
         </Tabs.List>
-      </Tabs>
+      </Tabs> */}
       <Grid p='lg' grow>
         <Grid.Col span={{xs: 8, sm: 6, md: 4}}>
           <Title>PEDRO E UN MMG (Aqui va la foto)</Title>
