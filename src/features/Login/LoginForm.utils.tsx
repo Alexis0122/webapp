06900b1ec95eb2a8ef4497715aca84a51fbc3a9d
Thunder04Wolf <pa.garcia0401@gmail.com>
2024-11-08 11:42:22 -0400

    feat: Add Layout Component

--- src/features/Login/LoginForm.utils.tsx
+++ src/features/Login/LoginForm.utils.tsx
@@ -1,8 +1,14 @@
-import { LoginForm } from "@/types/Project";
+import { LoginForm } from '@/types/Project'
 import * as yup from 'yup'
 import { object, ObjectSchema } from 'yup'
 
-export const LoginSchema: ObjectSchema<LoginForm> = object().shape({
-  email: yup.string().trim().required('Campo obligatorio'),
-  password: yup.string().trim().required('Campo obligatorio')
-}).required()
+export const LoginSchema: ObjectSchema<LoginForm> = object()
+  .shape({
+    email: yup.string().email('Correo no válido').trim().required('Campo obligatorio'),
+    password: yup
+      .string()
+      .trim()
+      .min(8, 'Debe tener al menos 8 caracteres')
+      .required('Campo obligatorio')
+  })
+  .required()
