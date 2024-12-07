42746a2e6e4ed3170013788fd36d7d91802434b7
FRAN-ARM <fran.arm.work@gmail.com>
2024-12-07 09:35:59 -0400

    About - Footer - Layout New BG - New Hook Function

--- src/features/Main/HomePage.tsx
+++ src/features/Main/HomePage.tsx
@@ -1,10 +1,33 @@
-import { Stack } from '@mantine/core'
+import { Group, Stack, Tabs } from '@mantine/core'
 import React from 'react'
 import { CampaignCarousel, CampagneCard, CampaignLanding } from '@/features/Main/components'
+import { Star } from '@phosphor-icons/react'
+import useNavigation from '@/hooks/useNavigation'
 
 export const HomePage = () => {
+  const { goTo } = useNavigation()
+
   return (
     <Stack gap='xl'>
+      <Group justify='center'>
+        <Tabs defaultValue='Home' variant='pills' radius='md'>
+          <Tabs.List>
+            <Tabs.Tab leftSection={<Star size={12} />} value='Home' onClick={() => goTo('/')}>
+              Home
+            </Tabs.Tab>
+            <Tabs.Tab
+              leftSection={<Star size={12} />}
+              value='Crowdfunding'
+              onClick={() => goTo('/Search/AllProjects')}
+            >
+              Crowdfunding
+            </Tabs.Tab>
+            <Tabs.Tab leftSection={<Star size={12} />} value='About' onClick={() => goTo('/about')}>
+              About
+            </Tabs.Tab>
+          </Tabs.List>
+        </Tabs>
+      </Group>
       <CampaignLanding />
       {/* <CampagneCard /> */}
       <CampaignCarousel />
