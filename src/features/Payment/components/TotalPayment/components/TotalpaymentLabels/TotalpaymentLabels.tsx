56f2a622587d7aa84829357fcd637bf7e0bc6a38
Thunder04Wolf <pa.garcia0401@gmail.com>
2024-12-07 14:27:35 -0400

    fix css

--- src/features/Payment/components/TotalPayment/components/TotalpaymentLabels/TotalpaymentLabels.tsx
+++ src/features/Payment/components/TotalPayment/components/TotalpaymentLabels/TotalpaymentLabels.tsx
@@ -1,10 +1,10 @@
-import React, { FC } from 'react';
-import './style.css';
-import { Input } from '@mantine/core';
+import React, { FC } from 'react'
+import './style.css'
+import { Input } from '@mantine/core'
 
 interface TotalPaymentLabelsProps {
   className: any
-  price:  number
+  price: number
   itbis: number
   billingDate: string
 }
@@ -14,61 +14,67 @@ const ARROWS = {
   price: 'https://c.animaapp.com/F8PWP7iM/img/arrow-9-1.svg',
   billingCycle: 'https://c.animaapp.com/F8PWP7iM/img/arrow-10-1.svg',
   nextBilling: 'https://c.animaapp.com/F8PWP7iM/img/arrow-11-1.svg',
-  perk: 'https://c.animaapp.com/F8PWP7iM/img/arrow-12-2.svg',
-};
+  perk: 'https://c.animaapp.com/F8PWP7iM/img/arrow-12-2.svg'
+}
 
 const STATIC_DATA = {
   plan: 'Classic',
   billingCycle: 'One Time',
   nextBillingDate: '10/23/2024',
-  perk: 'All Included',
-};
+  perk: 'All Included'
+}
 
-export const TotalPaymentLabels: FC<TotalPaymentLabelsProps> = ({ className, price, billingDate, itbis }) => {
+export const TotalPaymentLabels: FC<TotalPaymentLabelsProps> = ({
+  className,
+  price,
+  billingDate,
+  itbis
+}) => {
   return (
     <div className={`totalpayment-labels ${className}`}>
       {/* Plan Section */}
-      <div className="overlap-2">
-        <div className="text-wrapper-8">Plan</div>
-        <img className="arrow-2" alt="Arrow" src={ARROWS.default} />
+      <div className='overlap-2'>
+        <div className='text-wrapper-8'>Plan</div>
+        <img className='arrow-2' alt='Arrow' src={ARROWS.default} />
       </div>
-      <div className="text-wrapper-9">{STATIC_DATA.plan}</div>
+      <div className='text-wrapper-9'>{STATIC_DATA.plan}</div>
 
       {/* Price Section */}
-      <div className="overlap-3">
-        <div className="text-wrapper-8">Price</div>
-        <img className="arrow-3" alt="Arrow" src={ARROWS.price} />
+      {/* <div className='overlap-3'>
+        <div className='text-wrapper-8'>Price</div>
+        <img className='arrow-3' alt='Arrow' src={ARROWS.price} />
       </div>
-      <div className="text-wrapper-10">{`$ ${price}`}</div>
-      <div className="text-wrapper-11">{STATIC_DATA.billingCycle}</div>
+      <div className='text-wrapper-10'>{`$ ${price}`}</div>
+      <div className='text-wrapper-11'>{STATIC_DATA.billingCycle}</div> */}
 
       {/* Price Section */}
-      <div className="overlap-3">
-        <div className="text-wrapper-8">Itbis</div>
-        <img className="arrow-3" alt="Arrow" src={ARROWS.price} />
+      <div className='overlap-3'>
+        <div className='text-wrapper-8'>Itbis</div>
+        <img className='arrow-3' alt='Arrow' src={ARROWS.price} />
       </div>
-      <div className="text-wrapper-10">{`$ ${itbis}`}</div>
-      <div className="text-wrapper-11">{STATIC_DATA.billingCycle}</div>
+
+      <div className='text-wrapper-10'>{`$ ${itbis}`}</div>
+      <div className='text-wrapper-11'>{STATIC_DATA.billingCycle}</div>
 
       {/* Billing Cycle Section */}
-      <div className="overlap-4">
-        <div className="text-wrapper-8">Billing Cycle</div>
-        <img className="arrow-4" alt="Arrow" src={ARROWS.billingCycle} />
+      <div className='overlap-4'>
+        <div className='text-wrapper-8'>Billing Cycle</div>
+        <img className='arrow-4' alt='Arrow' src={ARROWS.billingCycle} />
       </div>
 
       {/* Next Billing Date Section */}
-      <div className="overlap-group-2">
-        <div className="text-wrapper-8">Next Billing Date</div>
-        <img className="arrow-5" alt="Arrow" src={ARROWS.nextBilling} />
+      <div className='overlap-group-2'>
+        <div className='text-wrapper-8'>Next Billing Date</div>
+        <img className='arrow-5' alt='Arrow' src={ARROWS.nextBilling} />
       </div>
-      <div className="text-wrapper-12">{billingDate}</div>
+      <div className='text-wrapper-12'>{billingDate}</div>
 
       {/* Selected Perk Section */}
-      <div className="overlap-5">
-        <div className="text-wrapper-8">Selected Perk</div>
-        <div className="text-wrapper-13">{STATIC_DATA.perk}</div>
-        <img className="arrow-6" alt="Arrow" src={ARROWS.perk} />
+      <div className='overlap-5'>
+        <div className='text-wrapper-8'>Selected Perk</div>
+        <div className='text-wrapper-13'>{STATIC_DATA.perk}</div>
+        <img className='arrow-6' alt='Arrow' src={ARROWS.perk} />
       </div>
     </div>
-  );
-};
+  )
+}
